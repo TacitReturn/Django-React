725b9b86ecd4fce8529ed5488a4c6ca3151a9099
TacitReturns <glennrudge@outlook.com>
2023-03-12 19:53:08 -0400

    Move core folder

--- django-api/core/user/models.py
+++ django-api/core/user/models.py
@@ -5,36 +5,12 @@ from django.http import Http404
 import uuid
 
 
-class User(AbstractBaseUser, PermissionsMixin):
-    public_id = models.UUIDField(
-        db_index=True, unique=True, default=uuid.uuid4, editable=False)
-    username = models.CharField(db_index=True, max_length=255, unique=True)
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    email = models.EmailField(db_index=True, unique=True)
-    is_active = models.BooleanField(default=True)
-    is_superuser = models.BooleanField(default=False)
-    created = models.DateTimeField(auto_new=True)
-    updated = models.DateTimeField(auto_new=True)
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
-    objects = UserManager()
-
-    def __str__(self):
-        return f"{self.email}"
-
-    @property
-    def name(self):
-        return f"{self.first_name} {self.last_name}"
-
-
 class UserManager(BaseUserManager):
     def get_objects_by_public_id(self, public_id):
         try:
             instance = self.get(public_id=public_id)
             return instance
-        except (ObjectDoesNotExists, ValueError, TypeError):
+        except (ObjectDoesNotExist, ValueError, TypeError):
             return Http404
 
     def create_user(self, username, email, password=None, **kwargs):
@@ -71,3 +47,27 @@ class UserManager(BaseUserManager):
         user.is_staff = True
         user.save(using=self._db)
         return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    public_id = models.UUIDField(
+        db_index=True, unique=True, default=uuid.uuid4, editable=False)
+    username = models.CharField(db_index=True, max_length=255, unique=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(db_index=True, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_new=True)
+    updated = models.DateTimeField(auto_new=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+    objects = UserManager()
+
+    def __str__(self):
+        return f"{self.email}"
+
+    @property
+    def name(self):
+        return f"{self.first_name} {self.last_name}"
