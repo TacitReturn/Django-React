2c01d2069104a037877b82fe71617c1b6780eb05
TacitReturns <glennrudge@outlook.com>
2023-03-12 22:33:48 -0400

    install serializer and djangorestframework, create UserSerializer viewsets, router.py

--- django-api/core/user/apps.py
+++ django-api/core/user/apps.py
@@ -3,5 +3,7 @@ from django.apps import AppConfig
 
 class UserConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
+    # name = 'user'
+    # label = 'user'
     name = 'core.user'
     label = 'core_user'
